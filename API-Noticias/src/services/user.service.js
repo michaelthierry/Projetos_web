2734d0b24bd69cee14802e9b0c4ef035cc4179a8
Michael Thierry <michaelthierry86@gmail.com>
2022-12-27 22:10:26 -0300

    feat: add function update user by Email

--- API-Noticias/src/services/user.service.js
+++ API-Noticias/src/services/user.service.js
@@ -8,9 +8,20 @@ const findAllService = () => User.find()
 
 const findByIdService = (id) =>User.findById(id)
 
+const updateUserByEmail = async (email, nome, username, password) => await User.findOneAndUpdate(
+    { email: email },
+    { nome: nome,
+      username:username,
+      password: password
+    },
+    {new:true}
+  );
+
+
 module.exports = {
     createService,
     findAllService,
     findByIdService,
-    deleteService
+    deleteService,
+    updateUserByEmail
 }
