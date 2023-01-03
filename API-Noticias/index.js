e72046427af6cb40834fdd878dbfddb8a0d5afa5
Michael Thierry <michaelthierry86@gmail.com>
2023-01-03 09:10:17 -0300

    refactor: add import type module

--- API-Noticias/index.js
+++ API-Noticias/index.js
@@ -1,15 +1,15 @@
 //Importando o express
-const express = require('express')
+import express from "express";
 //Importando as rotas
-const userRoute = require("./src/routes/user.route")
+import userRoute from "./src/routes/user.route.js"
 //Importando o conector do banco de dados
-const conectDataBase = require("./src/database/db")
+import connectDatabase from "./src/database/db.js";
 //Criando o App
 const app = express()
 //Porta a ser usada
 const port = 3000
 //conectadno a base de dados
-conectDataBase()
+connectDatabase()
 app.use(express.json())
 app.use("/user", userRoute)
 
