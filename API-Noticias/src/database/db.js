67352033e567b7d10a919370e76574f299301a69
Michael Thierry <michaelthierry86@gmail.com>
2023-01-03 11:10:00 -0300

    refactor: using ambient variable to datase connect

--- API-Noticias/src/database/db.js
+++ API-Noticias/src/database/db.js
@@ -6,7 +6,13 @@ const connectDatabase = () =>{
     //Mensagem para prompt
     console.log("Espere... estamos conectando ao banco")
     //Tenta conectar ao banco
-    mongoose.connect("mongodb+srv://michaelthierry:m1ch43l_TH@cluster0.u2xwqfd.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose
+    .connect(process.env.MONGODB_URI, 
+        {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        }
+    )
     .then(() => {
         //Mensagem caso de certo
         console.log("Mongo DB Atlas conectado!")
