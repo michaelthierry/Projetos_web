7d5c2be8db83a19cd5c575f6624e8e201966a67b
Michael Thierry <michaelthierry86@gmail.com>
2023-01-03 08:29:53 -0300

    refactor: add try catch in all functions

--- API-Noticias/src/middlewares/global.middlewares.js
+++ API-Noticias/src/middlewares/global.middlewares.js
@@ -2,25 +2,35 @@ const mongoose = require("mongoose");
 const userService = require("../services/user.service");
 
 const validId = (req, res, next) =>{
-    
-    const id = req.params.id;
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        //Retorna a mensagem de erro
-        return res.status(400).send({message: "Este id é invalido."})
-    }
 
-    next();
+    try{
+        const id = req.params.id;
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            //Retorna a mensagem de erro
+            return res.status(400).send({message: "Este id é invalido."})
+        }
+
+        next();
+    }catch(err){
+        res.status(500).send({message: err.message})
+    }    
+
 };
 
 const validUser = async (req, res, next) =>{
-    const id = req.params.id;
-    const user  = await userService.findByIdService(id);
-    if(!user){
-        return res.status(400).send({message: "Usuario não encontrado!"});
+    try{
+        const id = req.params.id;
+        const user  = await userService.findByIdService(id);
+        if(!user){
+            return res.status(400).send({message: "Usuario não encontrado!"});
+        }
+        req.id = id;
+        req.user = user;
+        next();
+    }catch(err){
+        res.status(500).send({message: err.message})
     }
-    req.id = id;
-    req.user = user;
-    next();
+    
 }
 
 module.exports = {
