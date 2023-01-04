22cc8fddecfb938dd1df3340819e82d2d2db5526
Michael Thierry <michaelthierry86@gmail.com>
2023-01-04 10:54:03 -0300

    feat: create authenfication user

---
+++ API-Noticias/src/controllers/auth.controller.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcrypt";
+import {loginService} from "../services/auth.service.js";
+const login = async (req, res) =>{
+    const {email, password} = req.body;
+    try{
+        const user = await loginService(email);
+        //res.send(user);
+
+        if(!user){
+            return res.status(404).send({message:"Senha ou usuario invalidos"});
+        }
+
+        const passwordIsvalid = bcrypt.compareSync(password, user.password)
+
+        if(!passwordIsvalid || !user){
+            return res.status(404).send({message: "Senha ou usuario invalidos"})
+        }
+        res.send("Login ok");
+    }catch(err){
+        res.status(500).send(err.message);
+    }
+    
+}
+
+export {login};
