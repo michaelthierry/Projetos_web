6c1cd6ab521767df1e51a3bd996d3524a5bbd9f0
Michael Thierry <michaelthierry86@gmail.com>
2023-01-04 10:52:36 -0300

    feat: add route authentificantion

--- API-Noticias/index.js
+++ API-Noticias/index.js
@@ -2,9 +2,9 @@
 import express from "express";
 //Importando as rotas
 import userRoute from "./src/routes/user.route.js"
+import authRoute from "./src/routes/auth.route.js"
 //Importando o conector do banco de dados
 import connectDatabase from "./src/database/db.js";
-
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -15,6 +15,7 @@ const port = process.env.PORT || 3000;
 //conectadno a base de dados
 connectDatabase()
 app.use(express.json())
-app.use("/user", userRoute)
+app.use("/user", userRoute);
+app.use('/auth', authRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
