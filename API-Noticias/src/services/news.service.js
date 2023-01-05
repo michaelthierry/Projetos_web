aa9477209d44c44ec09c95532df81216a4d1e177
Michael Thierry <michaelthierry86@gmail.com>
2023-01-05 13:06:20 -0300

    refactor: modify export default to export

--- API-Noticias/src/services/news.service.js
+++ API-Noticias/src/services/news.service.js
@@ -3,7 +3,7 @@ import News from "../models/News.js";
 const createService = (body) => News.create(body);
 const findAllService = () => News.find()
 
-export default {
+export {
     createService,
     findAllService
 }
