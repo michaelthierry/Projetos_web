caf50aa7077e2d8534690fb1d09f56ad6c348716
Michael Thierry <michaelthierry86@gmail.com>
2023-01-05 12:21:57 -0300

    feat: add route to news

--- API-Noticias/index.js
+++ API-Noticias/index.js
@@ -3,6 +3,7 @@ import express from "express";
 //Importando as rotas
 import userRoute from "./src/routes/user.route.js"
 import authRoute from "./src/routes/auth.route.js"
+import newsRoute from "./src/routes/news.route.js";
 //Importando o conector do banco de dados
 import connectDatabase from "./src/database/db.js";
 import dotenv from "dotenv";
@@ -17,5 +18,7 @@ connectDatabase()
 app.use(express.json())
 app.use("/user", userRoute);
 app.use('/auth', authRoute);
+app.use('/news', newsRoute);
+
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
