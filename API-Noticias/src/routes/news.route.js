9089346c2eeaf47ae7e4b55785204493b9098847
Michael Thierry <michaelthierry86@gmail.com>
2023-01-17 10:49:12 -0300

    feat: add authorization middleware in the route

--- API-Noticias/src/routes/news.route.js
+++ API-Noticias/src/routes/news.route.js
@@ -1,9 +1,9 @@
 import {Router} from "express";
 import {create, findAll} from "../controllers/news.controller.js"
-
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 const router = Router();
 //Rota para criar uma nova noticia
-router.post("/", create);
+router.post("/", authMiddleware,  create);
 //Rota pra pegar as noticias
 router.get("/", findAll);
 
