a15a01958cd87fe7f0cecda3391bf5df9b4243f9
Michael Thierry <michaelthierry86@gmail.com>
2023-01-27 10:14:01 -0300

    feat: add new rote to top news

--- API-Noticias/src/routes/news.route.js
+++ API-Noticias/src/routes/news.route.js
@@ -1,10 +1,12 @@
 import {Router} from "express";
-import {create, findAll} from "../controllers/news.controller.js"
+import {create, findAll, topNews} from "../controllers/news.controller.js"
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 const router = Router();
 //Rota para criar uma nova noticia
 router.post("/", authMiddleware,  create);
 //Rota pra pegar as noticias
 router.get("/", findAll);
+//Rota de topo de noticias
+router.get("/top", topNews);
 
 export default router;
