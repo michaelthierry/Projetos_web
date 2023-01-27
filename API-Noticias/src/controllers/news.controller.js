ff511b5d7176cf3be0b6fd36e0a8f3930b457a34
Michael Thierry <michaelthierry86@gmail.com>
2023-01-27 10:13:03 -0300

    refactore: format code

--- API-Noticias/src/controllers/news.controller.js
+++ API-Noticias/src/controllers/news.controller.js
@@ -1,11 +1,11 @@
-import {createService, findAllService, countNews} from "../services/news.service.js"
+import { createService, findAllService, countNews, topNewsService } from "../services/news.service.js"
 
 const create = async (req, res) => {
-    try{
-        const {title, text} = req.body;
-       
-        if(!title || !text){
-            req.status(400).send({message: "Submeta todos os campos para registrar"});
+    try {
+        const { title, text } = req.body;
+
+        if (!title || !text) {
+            res.status(400).send({ message: "Submeta todos os campos para registrar" });
         }
 
         await createService({
@@ -15,29 +15,30 @@ const create = async (req, res) => {
         })
         res.send(201);
 
-    }catch(err){
-        res.status(500).send({message: err.message});
+    } catch (err) {
+        res.status(500).send({ message: err.message });
     }
 
-   
+
 }
 
 const findAll = async (req, res) => {
-    let {limit, offset} = req.query;
-    
 
-    limit = Number(limit);
-    offset = Number(offset);
+    try {
 
-    if(!limit){
-        limit = 5;
-    }
+        let { limit, offset } = req.query;
 
-    if(!offset){
-        offset = 0;
-    }
+        limit = Number(limit);
+        offset = Number(offset);
 
-    try{
+        if (!limit) {
+            limit = 5;
+        }
+
+        if (!offset) {
+            offset = 0;
+        }
+
         const news = await findAllService(offset, limit);
         const total = await countNews();
         const currentURL = req.baseUrl;
@@ -48,37 +49,62 @@ const findAll = async (req, res) => {
         const next = offset + limit;
 
         const nextUrl = next < total ? `${currentURL}?limit=${limit}&offset=${next}` : null;
-        
-        const previous = offset - limit < 0 ? null: offset - limit;
-        const previousUrl = previous != null ? `${currentURL}?limit=${limit}&offset=${previous}`: null;
 
-        if(news.length === 0){
-            return res.status(400).send({message: "Não há noticias registradas!"});
+        const previous = offset - limit < 0 ? null : offset - limit;
+        const previousUrl = previous != null ? `${currentURL}?limit=${limit}&offset=${previous}` : null;
+
+        if (news.length === 0) {
+            return res.status(400).send({ message: "Não há noticias registradas!" });
         }
+
+        //news.map(item => console.log(JSON.stringify(item, null, 4)));
+
         res.send({
             nextUrl,
             previousUrl,
             limit,
             offset,
             total,
-            results: news.map(item => ({    
+            results: news.map(item => ({
                 id: item._id,
                 title: item.title,
                 text: item.text,
                 likes: item.likes,
                 comments: item.comments,
-                name: item.user.name,
-                unsername: item.user.unsername 
-            
-            }))
-        })
-    }catch(err){
-        res.status(500).send({message: err.message})
+                name: item.user.nome,
+                username: item.user.username
+            })),
+        });
+    } catch (err) {
+        res.status(500).send({ message: err.message })
     }
 }
 
+const topNews = async (req, res) => {
+    try {
+        const news = await topNewsService();
+        if (!news) {
+            return res.status(400).send({ message: "Não há post resgistrados!" });
+        }
+        res.send({
+            news: {
+                id: news._id,
+                title: news.title,
+                text: news.text,
+                likes: news.likes,
+                comments: news.comments,
+                name: news.user.nome,
+                username: news.user.username
+            }
+        })
+    } catch (err) {
+        res.status(500).send({ message: err.message })
+    }
+
+}
 
 export {
     create,
-    findAll
+    findAll,
+    topNews
 }
